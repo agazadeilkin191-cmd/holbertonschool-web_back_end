a84a5a1fa2042786723e057789a8e690df0e664a
agazadeilkin191-cmd <agazadeilkin191@gmail.com>
2026-07-17 14:11:14 +0300

    Refactor MongoDB client initialization and log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,21 +7,16 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    nginx_collection = db.nginx
+    nginx_collection = client.logs.nginx
 
-    # Total number of logs
     print("{} logs".format(nginx_collection.count_documents({})))
-
-    # Methods statistics
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, count))
 
-    # Status check
-    status_count = nginx_collection.count_documents(
+    status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print("{} status check".format(status_count))
+    print("{} status check".format(status_check))
