768c49aaadb746ee26ae136a3dff7a5b642fcb8c
agazadeilkin191-cmd <agazadeilkin191@gmail.com>
2026-07-29 13:39:41 +0300

    Add function to update student grades by city

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchedGrade = newGrades.find((g) => g.studentId === student.id);
+      return {
+        ...student,
+        grade: matchedGrade ? matchedGrade.grade : 'N/A',
+      };
+    });
+}
