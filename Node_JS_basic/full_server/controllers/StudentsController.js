2d8be43ab897ca234af72795ea0bb7e1cab58935
agazadeilkin191-cmd <agazadeilkin191@gmail.com>
2026-08-04 16:09:59 +0300

    Create StudentsController for student data retrieval
    Implement StudentsController with methods to get all students and filter by major.

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,41 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static async getAllStudents(req, res) {
+    const dbPath = process.argv[2];
+    try {
+      const students = await readDatabase(dbPath);
+      let responseText = 'This is the list of our students';
+      
+      const sortedFields = Object.keys(students).sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }));
+
+      sortedFields.forEach((field) => {
+        responseText += `\nNumber of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`;
+      });
+
+      res.status(200).send(responseText);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const dbPath = process.argv[2];
+    const { major } = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    try {
+      const students = await readDatabase(dbPath);
+      const majorStudents = students[major] || [];
+      res.status(200).send(`List: ${majorStudents.join(', ')}`);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+}
+
+export default StudentsController;
